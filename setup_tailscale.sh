944a496c03b83a3c327b47a932fdb7b13c35de4b
Elux414 <167685639+Elux414@users.noreply.github.com>
2024-06-20 06:43:24 +0300

    Update setup_tailscale.sh

--- setup_tailscale.sh
+++ setup_tailscale.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# START_FILE="/tmp/start_results.txt"
-# : > $START_FILE
-
-# Запуск Tailscale daemon
+# Запуск Tailscale daemon и socks5 прокси
 /usr/sbin/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 
 # Ожидание запуска Tailscale
@@ -11,20 +8,13 @@ sleep 10
 
 # Авторизация в Tailscale, используя ENV переменную TAILSCALE_AUTH_KEY
 tailscale up --authkey $TAILSCALE_AUTH_KEY --hostname=zabbix-neon
-tailscale ip -4
 
 # Ожидание, чтобы убедиться, что Tailscale поднят
 sleep 5
 
 echo "Tailscale setup completed."
 
-# CLOUD_USER="elux-bors@mail.ru"
-# CLOUD_PASSWORD="qfJricqHNkTspwD3pByU"
-# CLOUD_URL="https://webdav.mail.ru"
-
-# curl -u $CLOUD_USER:$CLOUD_PASSWORD -T $START_FILE "$CLOUD_URL/start_results.txt"
-
-# Бесконечный цикл с интервалом ожидания 1 день
+# Бесконечный цикл, чтобы процесс не упал в supervisor
 while true; do
-    sleep 86400 # Ожидание 1 день (86400 секунд)
+    sleep 86400
 done
